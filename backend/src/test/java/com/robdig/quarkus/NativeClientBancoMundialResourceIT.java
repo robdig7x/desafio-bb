c2515df72974b5e25e54619b54103e9cae291aea
robdig7x <robdig7x@gmail.com>
2022-06-06 14:48:09 -0300

    adicionando do swagger + update api 2.9.2.Final

--- backend/src/test/java/com/robdig/quarkus/NativeClientBancoMundialResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.robdig.quarkus;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeClientBancoMundialResourceIT extends ClientBancoMundialResourceTest {
-
-    // Execute the same tests but in native mode.
-}
